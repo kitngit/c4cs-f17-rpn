b67b583498f17f7a46ee2c08d95a3e25e5870ed8
Kit Ng <kitn@umich.edu>
2017-10-27 12:35:58 -0400

    final commit?

--- rpn.py
+++ rpn.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
+import operator
+ops = {
+	'+': operator.add,
+	'-': operator.sub
+}
+
 
 def calculate(myarg):
 	stack = list()
 	for token in myarg.split():
-		if token == '+':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 + arg2
-			stack.append(int(result))
-		elif token == '-':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 - arg2
-			stack.append(int(result))
-		else:	
+		try:
 			stack.append(int(token))
+		except ValueError:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			function = ops[token]
+			function(arg1, arg2)
+			result = function(arg1, arg2)
+			stack.append(result)
+
 	return stack.pop()
 
 def main():
